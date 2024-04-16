8a28db787f0da0a987525cc6bddcd9f318d8a1aa
interestingLSY <interestingLSY@gmail.com>
2024-04-16 23:49:09 +0800

    Fix filename for DistServe.png

--- src/data/publications.ts
+++ src/data/publications.ts
@@ -13,7 +13,7 @@ const publications: Publication[] = [
 		title: "DistServe: Disaggregating Prefill and Decoding for Goodput-optimized Large Language Model Serving",
 		authors: "Yinmin Zhong, <b>Shengyu Liu</b>, Junda Chen, Yibo Zhu, Xuanzhe Liu, Xin Jin, Hao Zhang",
 		time: "2023.12",
-		img_path: "/publications/DistAttention.png",
+		img_path: "/publications/DistServe.png",
 		description: "We propose DistServe, a novel large language model serving system that disaggregates prefill and decoding to optimize goodput under certain latency constraints (SLOs).",
 		submit_status: "In OSDI'24",
 		link: "https://arxiv.org/abs/2401.09670"
