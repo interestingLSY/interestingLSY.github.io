d66765a45c0e4f26adda61b9f22af8a06e2338ed
interestingLSY <interestingLSY@gmail.com>
2024-04-16 23:48:39 +0800

    Add new award: ASC 2024 champion

--- src/data/awards.ts
+++ src/data/awards.ts
@@ -6,49 +6,54 @@ interface Award {
 
 const awards: Award[] = [
 	{
-	  year: 2023,
-	  name: "Merit Student of Beijing",
-	  description: ""
+		year: 2024,
+		name: "Champion at the 10th ASC Student Supercomputer Challenge",
+		description: "As an assistant team member. ASC is The largest student supercomputer competition in the world."
 	},
 	{
-	  year: 2023,
-	  name: "Second place at the SC23 Student Cluster Competition",
-	  description: "As the <b>team leader</b>. SC (SuperComputing) is a world-famous conference for high-performance computing."
+		year: 2023,
+		name: "Merit Student of Beijing",
+		description: ""
 	},
 	{
-	  year: 2023,
-	  name: "The Highest Linpack Benchmark (HPL) Award at the SC23 Student Cluster Competition",
-	  description: ""
+		year: 2023,
+		name: "Second place at the SC23 Student Cluster Competition",
+		description: "As the <b>team leader</b>. SC (SuperComputing) is a world-famous conference for high-performance computing."
 	},
 	{
-	  year: 2023,
-	  name: "Community Impact Award at the SC23 Student Cluster Competition",
-	  description: ""
+		year: 2023,
+		name: "The Highest Linpack Benchmark (HPL) Award at the SC23 Student Cluster Competition",
+		description: ""
 	},
 	{
-	  year: 2023,
-	  name: "National Scholarship",
-	  description: "<b>Highest Honor</b> for undergraduates in China. <b>Top 1%</b> in Peking University."
+		year: 2023,
+		name: "Community Impact Award at the SC23 Student Cluster Competition",
+		description: ""
 	},
 	{
-	  year: 2023,
-	  name: "Pacemaker Award for Merit Student",
-	  description: "<b>Top 1%</b> in Peking University"
+		year: 2023,
+		name: "National Scholarship",
+		description: "<b>Highest Honor</b> for undergraduates in China. <b>Top 1%</b> in Peking University."
 	},
 	{
-	  year: 2022,
-	  name: "First place at the 10th ASC Student Supercomputer Challenge",
-	  description: "As the <b>team leader</b>. ASC is The largest student supercomputer competition in the world."
+		year: 2023,
+		name: "Pacemaker Award for Merit Student",
+		description: "<b>Top 1%</b> in Peking University"
 	},
 	{
-	  year: 2022,
-	  name: "John Hopcroft Scholarship of Peking University",
-	  description: ""
+		year: 2023,
+		name: "Champion at the 10th ASC Student Supercomputer Challenge",
+		description: "As the <b>team leader</b>. ASC is The largest student supercomputer competition in the world."
 	},
 	{
-	  year: 2022,
-	  name: "Academic Excellence Award",
-	  description: ""
+		year: 2022,
+		name: "John Hopcroft Scholarship of Peking University",
+		description: ""
+	},
+	{
+		year: 2022,
+		name: "Academic Excellence Award",
+		description: ""
 	},
 ];
 
