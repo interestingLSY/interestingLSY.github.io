7b09b486d4c2c8906a375aa5fba2a0d8f7087f76
interestingLSY <interestingLSY@gmail.com>
2024-04-17 16:18:16 +0800

    nits

--- src/data/publications.ts
+++ src/data/publications.ts
@@ -14,7 +14,7 @@ const publications: Publication[] = [
 		authors: "Bingyang Wu, <b>Shengyu Liu</b>, Yinmin Zhong, Peng Sun, Xuanzhe Liu, Xin Jin",
 		time: "2024.4",
 		img_path: "/publications/LoongServe.png",
-		description: "We propose a new parallelism paradigm, elastic sequence parallelism (ESP), to elastically adapt to the variance between different requests and phases during large language model (LLM) serving. We also design and build an inference system, LoongServe.",
+		description: "We propose a new parallelism paradigm, elastic sequence parallelism (ESP), to elastically adapt to the variance between different requests and phases during large language model (LLM) serving. We also design and build an inference system, LoongServe, based on that idea.",
 		submit_status: "In submission",
 		link: "https://arxiv.org/abs/2404.09526"
 	},
